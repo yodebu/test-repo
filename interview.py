837a27e7509093277772eae559e4bba2ce386c3f
Debapriya Das <yodebu@gmail.com>
2016-06-01 21:00:11 +0530

    test pycharm

---
+++ interview.py
@@ -0,0 +1,19 @@
+import MySQLdb as msql
+
+
+def connect_mysql(hostname, user, password):
+    '''
+
+    :param hostname: IP ADDRESS
+    :param user: username
+    :param password:
+    :return:
+    '''
+    conn = msql.connect(hostname, user, password)
+    return conn
+
+
+def check_innodb(conn):
+
+
+    return
