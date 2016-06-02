cb3645853524e80a50d7fbb2239f1aa2d1d760b3
Debapriya Das <yodebu@gmail.com>
2016-06-02 14:47:48 +0530

    test pycharm

--- mysqlconnect.py
+++ mysqlconnect.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
 import ConfigParser
+import sys
 
 import MySQLdb as msql
 
@@ -25,19 +30,148 @@ def config_read_file(filename='config.ini', section='mysql'):
     return db
 
 
-credentials = config_read_file()
-print credentials
+def check_innodb(cur):
+    #	mysql> select * from information_schema.engines;
+    #	+--------------------+---------+----------------------------------------------------------------+--------------+------+------------+
+    #	| ENGINE             | SUPPORT | COMMENT                                                        | TRANSACTIONS | XA   | SAVEPOINTS |
+    #	+--------------------+---------+----------------------------------------------------------------+--------------+------+------------+
+    #	| CSV                | YES     | CSV storage engine                                             | NO           | NO   | NO         |
+    #	| MRG_MYISAM         | YES     | Collection of identical MyISAM tables                          | NO           | NO   | NO         |
+    #	| MyISAM             | YES     | MyISAM storage engine                                          | NO           | NO   | NO         |
+    #	| BLACKHOLE          | YES     | /dev/null storage engine (anything you write to it disappears) | NO           | NO   | NO         |
+    #	| PERFORMANCE_SCHEMA | YES     | Performance Schema                                             | NO           | NO   | NO         |
+    #	| InnoDB             | DEFAULT | Supports transactions, row-level locking, and foreign keys     | YES          | YES  | YES        |
+    #	| ARCHIVE            | YES     | Archive storage engine                                         | NO           | NO   | NO         |
+    #	| MEMORY             | YES     | Hash based, stored in memory, useful for temporary tables      | NO           | NO   | NO         |
+    #	| FEDERATED          | NO      | Federated MySQL storage engine                                 | NULL         | NULL | NULL       |
+    #	+--------------------+---------+----------------------------------------------------------------+--------------+------+------------+
+    #	9 rows in set (0.00 sec)
 
-conn = msql.connect(**credentials)
-if conn:
-    print "Connected"
-    a = conn.cursor()
-    a.execute("show tables")
-    b = a.fetchall()
-    for i in b:
-        print i[0]
+    # hence we limit the Query to only the SUPPORT column where ENGINE = InnoDB
+    # output can be = DEFAULT, YES, NO or the Query can return NULL/EMPTY SET or 0 rows
+    # if the Engine InnoDB is not installed
 
-    a.execute("show tables")
-    b = a.fetchall()
-    for i in b:
-        print i[0]
+    sqlstring = "SELECT SUPPORT FROM INFORMATION_SCHEMA.ENGINES WHERE ENGINE = 'InnoDB';"
+    cur.execute(sqlstring)
+
+    # used fetchone() than fetchall() because the SQL query will by default return one row
+    res = cur.fetchone()
+    # converted tuple to string
+    res = str(res[0])
+    return res
+
+
+def find_total_write(cursor):
+    '''
+
+    :param cursor: MySQLdb Cursor() object
+    :return: total writes of disk
+    '''
+
+    # PERFORMANCE SCHEMA = mysql dev forums
+    # The Performance Schema monitors server events.
+    # An “event” is anything the server does that takes time
+    # and has been instrumented so that timing information can be collected.
+    #
+    # In general, an event could be a function call, a wait for the operating system,
+    # a stage of an SQL statement execution such as parsing or sorting, or an entire statement or group of statements.
+    # Event collection provides access to information about synchronization calls
+    # (such as for mutexes) and file I/O calls for the server and for several storage engines.
+
+    # File I/O Summaries:
+    #
+    #  file_summary_by_event_name: File events summarized per event name
+    #  file_summary_by_instance: File events summarized per file instance
+
+
+    # using two queries which calculate the total mySAM write operations and the InnoDB write operations
+
+
+    #
+    # mysql > select sum(count_write) from file_summary_by_instance where file_summary_by_instance.event_name like '%myISAM%';
+    # +------------------+
+    # | sum(count_write) |
+    # +------------------+
+    # | 0                |
+    # +------------------+
+    # 1 row in set(0.00 sec)
+    #
+    # Counts the total number of writes by the InnoDB
+    #
+    query = str(
+        "select cast(sum(count_write) as unsigned) from file_summary_by_instance where file_summary_by_instance.event_name like '%myISAM%';")
+    cursor.execute(query)
+    result_isam = cursor.fetchone()
+    result_isam = int(result_isam[0])  # isam_write count
+    print result_isam
+
+    # mysql > select sum(count_write) from file_summary_by_instance where file_summary_by_instance.event_name like '%InnoDB%';
+    # +------------------+
+    # | sum(count_write) |
+    # +------------------+
+    # | 56               |
+    # +------------------+
+    # 1 row in set(0.00 sec)
+    #
+    # Counts the total number of writes by the InnoDB
+    #
+    query2 = str(
+        "select cast(sum(count_write) as unsigned) from file_summary_by_instance where file_summary_by_instance.event_name like '%InnoDB%';")
+    cursor.execute(query2)
+    result_innodb = cursor.fetchone()
+    result_innodb = int(result_innodb[0])  # innodb_write count
+    print result_innodb
+    total = result_isam + result_innodb
+    print "total writes  = " + str(total)
+    return total
+
+
+def main():
+    '''
+    this is the main function of the program
+    :return:
+    '''
+
+    # # read the config.ini for the credentials
+    #
+    # we directly provide the db = performance_schema because
+    # we are reading variables from the performance_schema database
+
+    credentials = config_read_file()
+
+    try:
+        con = msql.connect(**credentials)
+        cur = con.cursor()
+
+        res = check_innodb(cur)
+        # the value returned to res will be a YES/NO/DEFAULT
+        # DEFAULT means the plugin InnoDB is the default storage engine and enabled by default
+        if res == 'YES' or res == 'DEFAULT':
+            print "InnoDB Plugin is Enabled"
+            c = find_total_write(cur)
+        elif res == 'NO':
+            print "InnoDB Module is not enabled"
+        else:
+            print "InnoDB Module is not installed"
+
+    except msql.Error, e:
+        # catches Error/Warnings on the MySQL command executions and logs them to the screen
+        # i.e. prints them to display
+        # and exits the program returning 1
+
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
+
+    finally:
+        # closing the connection after the work is done
+
+        if con:
+            con.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+def test_suite():
+    test_cases = ['default', 'yes', 'no', None]
--- testmysql.py
+++ testmysql.py
@@ -59,3 +59,81 @@ conn.close()
 # Innodb_log_writes - The number of physical writes to the log file
 #
 # Key_writes - The number of physical writes of a key block to disk
+
+
+
+
+# Show global status like '%write%' gives the following variables
+#
+#
+# +-----------------------------------+-------+
+# | Variable_name                     | Value |
+# +-----------------------------------+-------+
+# | Delayed_writes                    | 0     |
+# | Handler_write                     | 271   |
+# | Innodb_buffer_pool_write_requests | 515   |
+# | Innodb_data_pending_writes        | 0     |
+# | Innodb_data_writes                | 56    |
+# | Innodb_dblwr_writes               | 1     |
+# | Innodb_log_write_requests         | 0     |
+# | Innodb_log_writes                 | 2     |
+# | Innodb_os_log_pending_writes      | 0     |
+# | Innodb_truncated_status_writes    | 0     |
+# | Key_write_requests                | 0     |
+# | Key_writes                        | 0     |
+# +-----------------------------------+-------+
+# 12 rows in set (0.00 sec)
+#
+# as there was no defined parameter to find the total number of disk writes by MySQL, I assumed that it
+# should be the total sum of all the write variables -
+#
+#
+# this is what I got after a detailed study of the status variables -
+
+# Innodb_data_writes - The total number of data writes
+#
+# Innodb_dblwr_writes - The number of doublewrite operations that have been performed.
+#
+# Innodb_log_writes - The number of physical writes to the log file
+#
+# Key_writes - The number of physical writes of a key block to disk
+
+
+# * Innodb_data_fsyncs - number of fsync calls for data and log files
+#
+# * Innodb_log_writes - number of writes to the log file. These use buffered IO.
+#
+# * Innodb_os_log_fsyncs - number of fsync calls for the log file
+#
+# * Innodb_pages_read - number of reads done for data files.
+#                       These include single-page reads done by per-connection threads,
+#                       reads done by the main background thread during purge and
+#                       insert buffer merges and reads done by the background IO threads.
+#
+# * Innodb_data_reads - number of reads for data and log files.
+#                       Per-connection threads and the main background thread do reads in the size of an InnoDB page.
+#                       The background IO threads can merge adjacent requests and do reads
+#                       that are a multiple of the InnoDB page size.
+#
+# * Innodb_data_writes - number of writes for data and log files.
+#                       Most writes are done by the background IO threads
+#                       but in some cases per-connection threads request these writes
+#                       and then block until they are done.
+#                       This counter is incremented for the cheap log file writes
+#                       that are a multiple of 512 bytes and use buffered IO,
+#                       the frequently large writes to the double write buffer and the writes to the database files.
+#                       The database file writes are a multiple of the InnoDB page size as write merging can be done.
+#                       Some of the functions that request writes for dirty pages make it likely
+#                       that write requests can be merged.
+#                       Because this counter includes so many different types of writes
+#                       it is not easy to reason about the amount of disk IO that is done for a given value.
+#
+# * Innodb_pages_written - the number of data file pages written.
+#                           Writes to adjacent pages may be merged into a larger disk IO request.
+#
+# * Innodb_dblwr_pages_written - the number of data file pages written to the double write buffer.
+#
+# * Innodb_dblwr_writes - the number of disk writes done for the double write buffer.
+#                         When the double write buffer is enabled,
+#                         pages are first written to it sequentially using one large IO request
+#                         before being updated in place.
